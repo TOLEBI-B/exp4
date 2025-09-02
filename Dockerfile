838d2fd7cdf04fd0d0ccf7a66924d3d9db49f193
TOLEBI-B <tolebi.b@gmail.com>
2025-09-02 20:01:51 +0500

    2 commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+WORKDIR /app
+
+COPY go.mod ./
+COPY go.sum ./
+RUN go mod download
+
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
+FROM alpine:latest
+WORKDIR /root/
+COPY --from=builder /app/app .
+EXPOSE 9000
+CMD ["./app"]
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	router.GET("/api/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"greeting": "Hello from Go + Gin"})
+	})
+
+	router.GET("/env", func(c *gin.Context) {
+		dbUrl := os.Getenv("DATABASE_URL")
+		c.JSON(http.StatusOK, gin.H{"DATABASE_URL": dbUrl})
+	})
+
+	router.Run(":9001")
+}
